6eebd4e5babbc1dd6889d5aedc0644a3785838ba
anthonyle1 <leanthony2284@gmail.com>
2023-10-28 23:39:09 -0400

    created main() and menu() options

--- main.py
+++ main.py
@@ -7,3 +7,21 @@ def encoder(number):
             encoded_password += str(int(number[x]) + 3)
 
     return encoded_password
+
+def menu():
+    return "Menu \n------------- \n1. Encode \n2. Decode \n3. Quit" # shows the user menu options
+
+def main():
+    while True:
+        print(menu()) # prints menu
+        option = int(input("Choose an option:")) # asks user to choose an option, based on menu prompt
+        if option == 1:
+            print(encoder(input("Password:"))) # asks the user for their passwords, returns encrypted password to user
+        elif option == 2:
+            pass # FIXME : not completed
+        elif option == 3:
+            break # ends program
+
+
+if __name__ == "__main__":
+    main()
