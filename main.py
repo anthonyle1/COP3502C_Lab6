1c5539e17553942828e6b305595ae01545535469
andi0316 <andriasubhit@gmail.com>
2023-10-31 17:47:02 -0400

    Added the decode() function and menu option 2.

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-#Andria Subhit
+#   Anthony Le (owner), Andria Subhit (collaborator)
+
+#   Anthony
 def encoder(number):
     encoded_password = ""
     for x in range(len(number)): # password is normally 8 digits long
@@ -9,6 +11,25 @@ def encoder(number):
 
     return encoded_password
 
+#   Andria
+def decoder(number):
+
+    decoded_pass = ""
+
+    for i, num in enumerate(number):
+        # for values 3-9, subtract 3
+        if int(num) >= 3:
+            temp = (int(num) - 3)
+            decoded_pass += str(temp)
+            # print(str(num) + str(temp))
+            # print(i)
+        if int(num) < 3:
+            temp = (int(num) + 7)
+            decoded_pass += str(temp)
+
+    return decoded_pass
+
+#   Anthony
 def menu():
     return "Menu \n------------- \n1. Encode \n2. Decode \n3. Quit" # shows the user menu options
 
@@ -21,8 +42,9 @@ def main():
             # asks the user for their passwords, saves encrypted password
             encrypted_pw = encoder(input("Please enter your password to encode:"))
             print("Your password has been encoded and stored!")
-        elif option == 2:
-            pass # FIXME : not completed
+        elif option == 2:   # Andria
+            temp = decoder(encrypted_pw)
+            print(f"The encoded password is {encrypted_pw}, and the original password is {temp}")
         elif option == 3:
             break # ends program
         print("")
